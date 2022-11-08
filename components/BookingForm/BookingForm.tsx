ce7c5b441cb9052343d6484b210bcdbfc751baa4
qtd-trait <quang@traitsniper.com>
2022-11-08 11:39:03 +0700

    UI: Form static UI

--- components/BookingForm/BookingForm.tsx
+++ components/BookingForm/BookingForm.tsx
@@ -1,3 +1,53 @@
+import {
+  FaUser,
+  FaCalendarAlt,
+  FaCartPlus,
+  FaCreditCard,
+  FaRegMinusSquare,
+  FaRegPlusSquare,
+} from 'react-icons/fa';
+
 export const BookingForm = (): JSX.Element => {
-  return <div className="booking-form">Booking Form</div>;
+  return (
+    <div className="booking-form">
+      <div className="header">Booking Form</div>
+      <div className="input">
+        <div className="icon">
+          <FaUser />
+        </div>
+        <div className="placeholder">Number of Persons</div>
+        <div className="minus">
+          <FaRegMinusSquare />
+        </div>
+        <input type="number" min="0" className="people-no" value="0" />
+        <div className="plus">
+          <FaRegPlusSquare />
+        </div>
+      </div>
+      <div className="input">
+        <div className="icon">
+          <FaCalendarAlt />
+        </div>
+        <div className="placeholder">Select Date & Time</div>
+      </div>
+      <div className="input">
+        <div className="icon">
+          <FaCartPlus />
+        </div>
+        <div className="placeholder">Select Add-on(s)</div>
+      </div>
+
+      <div className="request">
+        <div className="min-price">
+          From <span>S$ 599 / group of 10</span>
+        </div>
+        <div className="btn">Request to Book</div>
+        <div className="payment">
+          <FaCreditCard />
+          Your request will be confirmed within 24h. Your card will be charged
+          on request confirmation.
+        </div>
+      </div>
+    </div>
+  );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -15,13 +15,12 @@ export default function Home() {
           crossOrigin="true"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;1,500&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;500;700&display=swap"
           rel="stylesheet"
         ></link>
       </Head>
 
       <main>
-        <h3>Seek Sophie take home test</h3>
         <BookingForm />
       </main>
     </div>
